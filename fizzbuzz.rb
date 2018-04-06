9a2481754c37ab0fe5de3722821dab0e831bab5f
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-04-06 21:00:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && i % 5 ! == 0)
+  if(int % 3 == 0 && i % 5 ! == 0)
     puts "Fizz"
-  elsif (int % 5 == 0 && i % 3 ! == 0)
+  elsif(int % 5 == 0 && i % 3 ! == 0)
     puts "Buzz"
-  else (int % 3 == 0 && int % 5 == 0)
+  else(int % 3 == 0 && int % 5 == 0)
     puts "FizzBuzz"
   end
 end
